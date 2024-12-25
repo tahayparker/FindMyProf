466a5985ae03a8f2cf2067a08d8be2e9dd3a23cd
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-25 15:15:15 +0400

    update

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = ({ children, onClick, isActive }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-3 py-1 rounded-full transition-all duration-200 ease-in-out border-2 hover:border-[#1E555C] ${
+        isActive ? 'border-[#1E555C] text-[#1E555C]' : 'border-[#D69F7E] text-white'
+      } hover:shadow-lg hover:shadow-[#1E555C]/50`}
+    >
+      <span className="text-sm font-semibold">{children}</span>
+    </button>
+  );
+};
+
+export default Button; 
